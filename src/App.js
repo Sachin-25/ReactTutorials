68ba7db6fbf97d6b9d0ee5a72c44a8cbd3064ebd
Sachey-25 <42082775+Sachey-25@users.noreply.github.com>
2021-09-27 18:36:43 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+     
+     <center> <h2>Hello to the world from React.Js</h2> </center>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const element = <h2>My name is Sachin</h2>
+ReactDOM.render(element, document.getElementById('root')); 
